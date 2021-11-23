222e0d8728973dc5462cf64088c32467118c67bf
Stevanus Prasetyo Soemadi <stevanus.pras@gmail.com>
2021-11-23 18:30:21 +0700

    init project

---
+++ kanban/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kanban
+//
+//  Created by Stevanus Prasetyo Soemadi on 23/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
